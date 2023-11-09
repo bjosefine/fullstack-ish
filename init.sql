f58e5cb8c14603fb5e74c108159802751c888421
bjosefine <josefinebrolin95@gmail.com>
2023-11-09 17:21:13 +0100

    insert into added

--- init.sql
+++ init.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS superheroes (
     id SERIAL PRIMARY KEY,
-    name text UNIQUE NOT NULL,
+    "name" text UNIQUE NOT NULL,
     power text
-)
+);
 
--- INSERT INTO superheroes (name, power)
--- VALUES ('Spider-Man', 'Web slinging'),
--- ('Sfsal', 'lkfjl')
+INSERT INTO superheroes ("name", power)
+VALUES ('Spider-Man', 'Web slinging');
